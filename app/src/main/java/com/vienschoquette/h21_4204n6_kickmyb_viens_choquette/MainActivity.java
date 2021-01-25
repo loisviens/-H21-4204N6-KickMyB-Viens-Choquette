5ff5db60a9eef711fa0796f3c149352bccaa95e6
lois Viens <lois.v.c@hotmail.com>
2021-01-25 13:47:50 -0500

    FCT
    Addition des fichiers de base. Aucun travail n’a été commencé.

---
+++ app/src/main/java/com/vienschoquette/h21_4204n6_kickmyb_viens_choquette/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vienschoquette.h21_4204n6_kickmyb_viens_choquette;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
